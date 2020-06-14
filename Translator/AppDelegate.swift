0d6dafb54d79d598757bc32b7c513f0b6c17533c
Сергей Полозов <polozov@surfstudio.co>
2020-06-14 17:30:14 +0300

    Added the project

---
+++ Translator/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Translator
+//
+//  Created by Сергей Полозов on 14.06.2020.
+//  Copyright © 2020 Сергей Полозов. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Translator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by Сергей Полозов on 14.06.2020.
+//  Copyright © 2020 Сергей Полозов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
